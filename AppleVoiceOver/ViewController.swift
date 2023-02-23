da507db4e0ddb011c78dc8f50302fc525ff5df20
harundemirkaya <harundmrk@gmail.com>
2023-02-23 11:41:42 +0300

    Initial Commit

---
+++ AppleVoiceOver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleVoiceOver
+//
+//  Created by Harun Demirkaya on 23.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
